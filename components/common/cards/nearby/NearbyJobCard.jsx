6d798e7f85c29854fa782e948efc1bf456df24bb
mbartos <maciekbartos8@gmail.com>
2023-04-13 18:35:34 +0200

    Add nearby job card component

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,35 @@
+import { Image, View, Text, TouchableOpacity } from 'react-native';
+
+import styles from './nearbyjobcard.style';
+const NearbyJobCard = ({ job, handleNavigate }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={handleNavigate}
+    >
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: job.employer_logo
+              ? job.employer_logo
+              : 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+          }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text
+          style={styles.jobName}
+          numberOfLines={1}
+        >
+          {job.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NearbyJobCard;
