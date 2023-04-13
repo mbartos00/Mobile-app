ff49c05027515ddf1d2ebba522dc870b986c6e91
mbartos <maciekbartos8@gmail.com>
2023-04-13 19:26:56 +0200

    Add job details

---
+++ components/jobdetails/company/Company.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import styles from './company.style';
+import { icons } from '../../../constants';
+
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: companyLogo
+              ? companyLogo
+              : 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg',
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Company;
